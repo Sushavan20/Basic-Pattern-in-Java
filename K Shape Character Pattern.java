ebfd788a407d68fd10999c6527870e425b2430c7
Sushavan Karmakar <97795991+Sushavan20@users.noreply.github.com>
2023-04-20 11:01:13 +0530

    Update and rename p20.java to K Shape Character Pattern.java

--- K Shape Character Pattern.java
+++ K Shape Character Pattern.java
@@ -20,7 +20,7 @@ A B C D E F
 
 import java.util.Scanner;
  
-public class p20{
+public class Test{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
