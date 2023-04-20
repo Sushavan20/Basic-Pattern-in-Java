bbcd38131197fe6db712befe5273b26ca1d4efa8
Sushavan Karmakar <97795991+Sushavan20@users.noreply.github.com>
2023-04-20 10:55:48 +0530

    Update and rename p10.java to Hollow Diamond Star Pattern.java

--- Hollow Diamond Star Pattern.java
+++ Hollow Diamond Star Pattern.java
@@ -15,8 +15,7 @@ Pattern
 
 
 import java.util.Scanner;
-public class p10
-{
+public class Test{
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter number of rows for one half: ");
